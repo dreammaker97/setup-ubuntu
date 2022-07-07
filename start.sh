31367d806d6227add8f924577361a091ed682af6
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2022-07-07 15:00:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get update && sudo apt-get upgrade
+apt-get update && apt-get upgrade
 apt-get install curl apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
