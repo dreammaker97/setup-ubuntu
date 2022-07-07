4540b8d7623a4d4caf29837f981c6a17cb02205c
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2022-07-07 14:53:45 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt-get install docker-ce
+
+sudo apt install docker-compose
+
+git clone https://github.com/dreammaker97/portainer
+
+cd portainer
+
+sudo docker-compose up
+
+sudo apt install tree
